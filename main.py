5e48555e02e1034d13f5814a692a026dfca1e2bb
james1236 <32351696+james1236@users.noreply.github.com>
2021-02-25 01:23:33 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from ssd1306 import SSD1306_I2C
 from time import sleep
 import os
 
+import framebuf
+
+#Stop any previous music PWM
+from buzzer_music import music
+music().stop()
+
 width = 128
 height = 64
 
@@ -14,7 +20,7 @@ height = 64
 i2c = I2C(0,sda=Pin(4),scl=Pin(5),freq=600000) #40000
 oled = SSD1306_I2C(width,height,i2c)
 
-#Display wrapper class
+#Display wrapper class - better than display specific draw instructions...
 class display:
     def __init__(self, oled, display_type):
         self.oled = oled
@@ -26,24 +32,32 @@ class display:
         if (self.display_type == "SSD1306 Monochrome"):
             self.oled.fill_rect(x,y,width,height,color)
         else:
-            #Can add support for other screens
+            #...since you can add support for other screens
             pass
         
     def show(self):
         if (self.display_type == "SSD1306 Monochrome"):
-            oled.show()
+            self.oled.show()
         
     def fill(self, color):
         if (self.display_type == "SSD1306 Monochrome"):
-            oled.fill(color)
+            self.oled.fill(color)
             
     def text(self, text, x, y):
         if (self.display_type == "SSD1306 Monochrome"):
-            oled.text(text, x, y)
+            self.oled.text(text, x, y)
     
     def pixel(self, x, y, color):
         if (self.display_type == "SSD1306 Monochrome"):
-            oled.pixel(x, y, color)
+            self.oled.pixel(x, y, color)
+            
+    def blit(self, buffer, x, y, color):
+        if (self.display_type == "SSD1306 Monochrome"):
+            self.oled.blit(buffer, x, y, color)
+            
+    def line(self, x1, y1, x2, y2, color):
+        if (self.display_type == "SSD1306 Monochrome"):
+            self.oled.line(x1, y1, x2, y2, color)
         
     def stroke_rect(self, x, y, width, height, color):
         self.fill_rect(0+x,0+y,width,1,color)
@@ -61,21 +75,77 @@ cursor = 0
 
 frame = 0
 
+names = []
+thumbnails = []
+
+#Get name+thumbnail for each creation
+i = 0
+for filename in creations:
+    #Attempt Import
+    exec("try:\n    from creations."+creations[i].replace('.py','')+" import description\nexcept ImportError:\n    pass")
+    
+    try:
+        description
+    except NameError:
+        #Unsuccessful
+        names.append(creations[i].replace('.py',''))
+        thumbnails.append(None)
+    else:
+        #Successful
+        if (hasattr(description, 'name')):
+            names.append(description.name)
+        else:
+            names.append(creations[i].replace('.py',''))
+            
+        if (hasattr(description, 'thumbnail')):
+            thumbnails.append(framebuf.FrameBuffer(description.thumbnail, 32, 32, framebuf.MONO_HLSB))
+        else:
+            thumbnails.append(None)
+        
+        del description
+    i = i + 1
+
+#Function to load into the selected creation
 def onSelect():
+    global creations
+    global cursor
+    global creation
+    global mainMenu
+    global display
+    global config
+    
+    display.fill(0)
+    display.text("Loading %s..." % creations[cursor].replace('.py',''), 0, 0)
+    display.show()
+    
+    autorunFile = open('autorun', 'w')
+    autorunFile.write(str(cursor))
+    autorunFile.close()
+    
     exec("from creations.%s import creation" % creations[cursor].replace('.py',''))
     creation = creation(display)
     mainMenu = False
-
-while True:
-    display.fill(0)
     
+#Try to auto open previous creation
+autorunFile = open('autorun', 'r')
+c = autorunFile.read()
+if (not (c == '')):
+    cursor = int(c)
+    onSelect()
+autorunFile.close()
+
+while True:    
     if (mainMenu):
+        display.fill(0)
+        if (not (thumbnails[cursor] is None)):
+            display.blit(thumbnails[cursor],display.width-40,16,0)
+        
         #List files in creations folder
         ySpacing = 11
         i = 0
             
         for filename in creations:
-            display.text(filename.replace('.py',''),9,3+(ySpacing*i))
+            display.text(names[i],9,3+(ySpacing*i))
             
             #Selection Border
             if (cursor == i):
@@ -91,25 +161,8 @@ while True:
         display.fill_rect(1,round(position*display.height)-round(display.height/8),2,round(display.height/4),1)
                 
         display.show()
-            
+        
     else:
         #Run loaded creation
         creation.tick()
         sleep(0.0166)
-
-
-#Draw Examples
-
-#oled.fill_rect(20,20,50,10,1)
-
-#oled.contrast(255)
-#oled.pixel(20,20,1)
-
-#oled.invert(1)
-#oled.invert(0)
-        
-#File Example
-        
-#f = open('/creations/log.txt', 'w')
-#f.write('some data')
-#f.close()
